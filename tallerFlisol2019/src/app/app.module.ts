d9b2dd6bda4c1c9152dcbb4e267d9fa2040e2915
tejo8003 <josuesalvador.tejocote.consultant@nielsen.com>
2019-04-05 11:50:15 -0600

    componentes anguñar con ngx bootstrap

--- tallerFlisol2019/src/app/app.module.ts
+++ tallerFlisol2019/src/app/app.module.ts
@@ -2,13 +2,47 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AccordionModule } from 'ngx-bootstrap/accordion';
+import { TypeaheadModule } from 'ngx-bootstrap/typeahead';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { TimepickerModule } from 'ngx-bootstrap/timepicker';
+import { TabsModule } from 'ngx-bootstrap/tabs';
+import { SortableModule } from 'ngx-bootstrap/sortable';
+import { RatingModule } from 'ngx-bootstrap/rating';
+import { ProgressbarModule } from 'ngx-bootstrap/progressbar';
+import { PopoverModule } from 'ngx-bootstrap/popover';
+import { PaginationModule } from 'ngx-bootstrap/pagination';
+import { ModalModule } from 'ngx-bootstrap/modal';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
+import { CollapseModule } from 'ngx-bootstrap/collapse';
+import { CarouselModule } from 'ngx-bootstrap/carousel';
+import { ButtonsModule } from 'ngx-bootstrap/buttons';
+import { AlertModule } from 'ngx-bootstrap/alert';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AccordionModule.forRoot(),
+    TypeaheadModule.forRoot(),
+    TooltipModule.forRoot(),
+    TimepickerModule.forRoot(),
+    TabsModule.forRoot(),
+    SortableModule.forRoot(),
+    RatingModule.forRoot(),
+    ProgressbarModule.forRoot(),
+    PopoverModule.forRoot(),
+    PaginationModule.forRoot(),
+    ModalModule.forRoot(),
+    BsDropdownModule.forRoot(),
+    BsDatepickerModule.forRoot(),
+    CollapseModule.forRoot(),
+    CarouselModule.forRoot(),
+    ButtonsModule.forRoot(),
+    AlertModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
